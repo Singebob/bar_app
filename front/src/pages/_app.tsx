b25869a301f18d364ff28e9b85352d72b93bb52e
jpiazzal <joel.piazzalungalerat@gmail.com>
2023-04-24 17:17:13 +0200

    refactor: Move head and remove some useless style

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,11 +1,19 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <ChakraProvider>
-            <Component {...pageProps} />
-        </ChakraProvider>
+        <>
+            <Head>
+                <title>Bar</title>
+            </Head>
+            <ChakraProvider>
+                <main>
+                    <Component {...pageProps} />
+                </main>
+            </ChakraProvider>
+        </>
     );
 }
 
--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,20 +1,3 @@
-import { Button } from '@chakra-ui/react';
-import { Inter } from 'next/font/google';
-import Head from 'next/head';
-
-import styles from '@/styles/Home.module.css';
-
-const inter = Inter({ subsets: ['latin'] });
-
 export default function Home() {
-    return (
-        <>
-            <Head>
-                <title>Bars</title>
-            </Head>
-            <main className={`${styles.main} ${inter.className}`}>
-                <Button>Test</Button>
-            </main>
-        </>
-    );
+    return <h1>Bar</h1>;
 }
