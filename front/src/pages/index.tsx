05d806fbc1da69441594cca084a04f36b25d47fc
jpiazzal <joel.piazzalungalerat@gmail.com>
2023-04-25 15:37:40 +0200

    feat: add drink cards and tests with playwright

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,25 +1,103 @@
 import { AddIcon } from '@chakra-ui/icons';
-import { Alert, AlertIcon, AlertTitle, Button, Center } from '@chakra-ui/react';
+import {
+    Alert,
+    AlertIcon,
+    AlertTitle,
+    Button,
+    Card,
+    CardBody,
+    Center,
+    Grid,
+    Heading,
+    Img,
+    Spinner,
+    Stack,
+    Tag,
+    Text,
+} from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+
+import { Drink } from '@/types/bar';
 
 export default function Home() {
+    const [drinks, setDrinks] = useState<Drink[] | null>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+
+    const fetchDrinks = async () => {
+        const response = await fetch('/api/drinks?page=1&size=2');
+        const data = await response.json();
+        setDrinks(data);
+        setIsLoading(false);
+    };
+
+    useEffect(() => {
+        fetchDrinks();
+    }, []);
+
+    if (isLoading) {
+        return (
+            <Center>
+                <Spinner thickness="4px" speed="0.65s" emptyColor="gray.200" color="blue.500" size="xl" />
+            </Center>
+        );
+    }
+
     return (
         <>
-            <Alert
-                status="info"
-                flexDirection="column"
-                alignItems="center"
-                justifyContent="center"
-                maxWidth={400}
-                mx={'auto'}
-                textAlign="center"
-                rounded={5}
-            >
-                <AlertIcon />
-                <AlertTitle>Pas de boisson disponible pour le moment.</AlertTitle>
-            </Alert>
+            {drinks && drinks.length > 0 ? (
+                <Grid
+                    templateColumns={{
+                        base: 'repeat(1, 1fr)',
+                        sm: 'repeat(2, 1fr)',
+                        md: 'repeat(3, 1fr)',
+                    }}
+                    autoFlow={{ base: 'row', md: 'column' }}
+                    autoColumns={{ base: 'auto', md: '1fr' }}
+                    gap={6}
+                >
+                    {drinks &&
+                        drinks.map((drink) => (
+                            <Card key={drink.name} data-testid={drink.name} maxWidth={300}>
+                                <CardBody>
+                                    {drink.image ? (
+                                        <Img src={drink.image} alt={drink.name} />
+                                    ) : (
+                                        <Center height={200}>Pas d&apos;image</Center>
+                                    )}
+                                    <Stack mt={6} spacing={3}>
+                                        <Stack direction="row" alignItems="center">
+                                            <Heading as="h3">{drink.name}</Heading>
+                                            <Text color="blue.600" fontSize="2x">
+                                                {drink.price.toFixed(2)} €
+                                            </Text>
+                                        </Stack>
+                                        <Tag size="lg" maxWidth="min-content">
+                                            {drink.category}
+                                        </Tag>
+                                    </Stack>
+                                </CardBody>
+                            </Card>
+                        ))}
+                </Grid>
+            ) : (
+                <Alert
+                    status="info"
+                    flexDirection="column"
+                    alignItems="center"
+                    justifyContent="center"
+                    maxWidth={400}
+                    mx={'auto'}
+                    textAlign="center"
+                    rounded={5}
+                >
+                    <AlertIcon />
+                    <AlertTitle>Pas de boisson disponible pour le moment.</AlertTitle>
+                </Alert>
+            )}
+
             <Center marginTop={4}>
                 <Button leftIcon={<AddIcon />} colorScheme="teal" variant="solid" rounded={5}>
-                    Ajouter votre première boisson
+                    {drinks && drinks.length > 0 ? 'Ajouter une boisson' : 'Ajouter votre première boisson'}
                 </Button>
             </Center>
         </>
--- front/tests/home.spec.ts
+++ front/tests/home.spec.ts
@@ -1,11 +1,83 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Locator } from '@playwright/test';
+
+import { Drink } from '@/types/bar';
+
+const drinks = [
+    {
+        name: 'Coca',
+        category: 'Soda',
+        price: 2.5,
+        image: 'https://i0.wp.com/commechezmams.fr/wp-content/uploads/2021/01/COCA-33cl.jpg?fit=800%2C800&ssl=1',
+    },
+    {
+        name: 'Orangina',
+        category: 'Soda',
+        price: 2.5,
+        image: 'https://www.coursesu.com/media/produit/300x300/orangina-boisson-gazeuse-a-l-orange-33cl-0-33l-0-33l-1-1.jpg',
+    },
+    {
+        name: 'Ice Tea',
+        category: 'Soda',
+        price: 2.5,
+        image: 'https://www.coursesu.com/media/produit/300x300/ice-tea-peche-boisson-gazeuse-au-the-peche-33cl-0-33l-0-33l-1-1.jpg',
+    },
+];
+
+async function expectDrink(locator: Locator, drink: Drink) {
+    await expect(locator.getByRole('heading', { name: drink.name, level: 3 })).toBeVisible();
+    await expect(locator.getByText(drink.category)).toBeVisible();
+    await expect(locator.getByText(`${drink.price.toFixed(2)} €`)).toBeVisible();
+    await expect(locator.getByAltText(drink.name)).toBeVisible();
+}
+
+const API_PATH = '**/api/drinks';
+const CONTENT_TYPE = 'application/json';
 
 test('when no drinks are available, the user is informed with a alert', async ({ page }) => {
+    await page.route(API_PATH, async (route) => {
+        await route.fulfill({
+            status: 200,
+            contentType: CONTENT_TYPE,
+            body: JSON.stringify([]),
+        });
+    });
     await page.goto('/');
 
     await expect(
         page.getByRole('alert').filter({ hasText: 'Pas de boisson disponible pour le moment.' }),
     ).toBeVisible();
     await expect(page.getByRole('button', { name: 'Ajouter votre première boisson' })).toBeVisible();
-    // await expect(page.getByRole('button', { name: 'Ajouter votre première boisson' })).toHaveAttribute('href', '#');
+});
+
+test('when one drink is available, the user can see it', async ({ page }) => {
+    const drink = drinks[0];
+    await page.route(API_PATH, async (route) => {
+        await route.fulfill({
+            status: 200,
+            contentType: CONTENT_TYPE,
+            body: JSON.stringify([drink]),
+        });
+    });
+    await page.goto('/');
+
+    const cardDrink = await page.getByTestId(drink.name);
+    await expectDrink(cardDrink, drink);
+    await expect(page.getByRole('button', { name: 'Ajouter une boisson' })).toBeVisible();
+});
+
+test('when multiple drinks are available, the user can see them', async ({ page }) => {
+    await page.route(API_PATH, async (route) => {
+        await route.fulfill({
+            status: 200,
+            contentType: CONTENT_TYPE,
+            body: JSON.stringify(drinks),
+        });
+    });
+    await page.goto('/');
+
+    for (const drink of drinks) {
+        const cardDrink = await page.getByTestId(drink.name);
+        await expectDrink(cardDrink, drink);
+    }
+    await expect(page.getByRole('button', { name: 'Ajouter une boisson' })).toBeVisible();
 });
