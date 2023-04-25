4a3ebb67b91033bbe8183472a33072abe8cb1f89
jpiazzal <joel.piazzalungalerat@gmail.com>
2023-04-25 18:05:39 +0200

    draft: test display toast on button click

--- front/tests/route.spec.ts
+++ front/tests/route.spec.ts
@@ -1,12 +1,22 @@
-import { expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 import { testMockOneDrink } from './fixtures/api.mock';
+
+const ADD_DRINK_PATH = '/drinks/add';
 
 testMockOneDrink('should be able to route from home to add drink', async ({ page }) => {
     await page.goto('/');
 
     expect(page.getByRole('button', { name: 'Ajouter une boisson' })).toBeVisible();
     await page.getByRole('button', { name: 'Ajouter une boisson' }).click();
-    await page.waitForURL('/drinks/add');
-    expect(page.url()).toContain('/drinks/add');
+    await page.waitForURL(ADD_DRINK_PATH);
+    expect(page.url()).toContain(ADD_DRINK_PATH);
+});
+
+test.skip('should display toast when drink is added', async ({ page }) => {
+    await page.goto(ADD_DRINK_PATH);
+
+    await page.getByRole('button', { name: 'Ajouter' }).click();
+
+    expect(page.getByText('Pas implémenté')).toBeVisible();
 });
